b5fa82356832181a2f39df8e360f40dfc034eef1
JBOuser <viscaelsbarcelonistes@gmail.com>
2022-09-17 13:21:04 +0200

    v1.2. Changed <BrowserRouter> (dev) by <HashRouter> (prod)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 //import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, //used in a 'development' environment
+  HashRouter, //used in a 'production' environment
+  Switch, Route 
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Signin from './pages/SignIn/signin';
 import { trimLine,asPath } from './utils';
@@ -10,13 +14,15 @@ import { SIGN_IN } from './components/Navbar/NavbarConstants';
 class App extends Component{
   render(){
     return (
-      <Router>
+      // <Router>
+      <HashRouter>
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path={asPath(trimLine(SIGN_IN))} component={Signin} exact />
         </Switch>
         {/* <Home/> */}
-      </Router>
+      </HashRouter>
+      // </Router>
     );
   }
 }
