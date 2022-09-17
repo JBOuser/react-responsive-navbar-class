4c14c9b39db8389e08b85256e7b573bff443b516
JBOuser <viscaelsbarcelonistes@gmail.com>
2022-09-17 13:44:37 +0200

    Updates

---
+++ precache-manifest.419186ae19caf672a295363646a4c02c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c5b5eebb84374ceafdce48d53569ce",
+    "url": "/react-responsive-navbar-class/index.html"
+  },
+  {
+    "revision": "2098bf79ff3822f2c2da",
+    "url": "/react-responsive-navbar-class/static/css/main.cb669b7f.chunk.css"
+  },
+  {
+    "revision": "e9acf7a9b9faf7a7469b",
+    "url": "/react-responsive-navbar-class/static/js/2.f44cf4c0.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/react-responsive-navbar-class/static/js/2.f44cf4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2098bf79ff3822f2c2da",
+    "url": "/react-responsive-navbar-class/static/js/main.f461b16f.chunk.js"
+  },
+  {
+    "revision": "51e1bfd90308252972d6",
+    "url": "/react-responsive-navbar-class/static/js/runtime-main.c37e380f.js"
+  },
+  {
+    "revision": "b5a70d6c0f027ed09d63fb8268d954c2",
+    "url": "/react-responsive-navbar-class/static/media/first_video.b5a70d6c.mp4"
+  },
+  {
+    "revision": "7d3e4227919ce7587ecfe178e7988013",
+    "url": "/react-responsive-navbar-class/static/media/s1.7d3e4227.svg"
+  },
+  {
+    "revision": "1fd3d4af1004d5f8e8a38c6a4ce1c64a",
+    "url": "/react-responsive-navbar-class/static/media/s2.1fd3d4af.svg"
+  },
+  {
+    "revision": "0bfaa3efedd8f12bc025b96441a5e125",
+    "url": "/react-responsive-navbar-class/static/media/s3.0bfaa3ef.svg"
+  },
+  {
+    "revision": "920f08d26ee42da067b2edb079f12047",
+    "url": "/react-responsive-navbar-class/static/media/s4.920f08d2.svg"
+  },
+  {
+    "revision": "183a450983b7e9156661eb63233c85d7",
+    "url": "/react-responsive-navbar-class/static/media/s5.183a4509.svg"
+  },
+  {
+    "revision": "d724988870ec9cfb10779b6231a94ecb",
+    "url": "/react-responsive-navbar-class/static/media/s6.d7249888.svg"
+  },
+  {
+    "revision": "f34d65cb3ddeb8b21d13bae098bf1c47",
+    "url": "/react-responsive-navbar-class/static/media/white_rabbit_64.f34d65cb.svg"
+  }
+]);
